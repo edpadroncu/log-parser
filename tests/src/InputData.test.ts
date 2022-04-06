31416dfb3b628788c9f68252301585612daec291
Enrique <enrique.diaz@dox.cl>
2022-04-05 21:22:43 -0400

    fix test path creation

--- tests/src/InputData.test.ts
+++ tests/src/InputData.test.ts
@@ -2,45 +2,36 @@ import { InputData } from '../../src/model/idata'
 import * as fs from 'fs'
 
 describe('InputData', () => {
-    describe('isInputValid', () => {
-        const input_cmd = `${require('os').homedir()}/app-logs/myapp.log`
-        
-        beforeEach(() => {
-            if (!fs.existsSync(input_cmd))
-                fs.writeFileSync(input_cmd, '')
-        })
+    const directory_path = `${require('os').homedir()}/app-logs`
+    const input_cmd = `${directory_path}/myapp.log`
+    const output_cmd = `${directory_path}/myapp_errors.json`
 
+    beforeEach(() => {
+        if (!fs.existsSync(directory_path))
+            fs.mkdirSync(directory_path)
+        if (!fs.existsSync(input_cmd))
+            fs.writeFileSync(input_cmd, '')
+    })
+
+    describe('isInputValid', () => {
         it('should return true', () => {
             expect(new InputData(input_cmd, "").isInputValid).toBeTruthy()
         })
     })
 
     describe('isOutputValid', () => {
-        const directory_path = `${require('os').homedir()}/app-logs`
-        const output_cmd = `${directory_path}/myapp_errors.json`
-        
-        beforeEach(() => {
-            if (!fs.existsSync(directory_path))
-                fs.mkdirSync(directory_path)
-        })
-
         it('should return true', () => {
             expect(new InputData('', output_cmd).isOutputValid).toBeTruthy()
         })
     })
 
     describe('createOutputFile', () => {
-        const directory_path = `${require('os').homedir()}/app-logs/`
-        const output_cmd = `${directory_path}/myapp_errors.json`
-        
         beforeEach(() => {
-            if (!fs.existsSync(directory_path))
-                fs.mkdirSync(directory_path)
+            const idata = new InputData('', output_cmd)
+            idata.createOutputFile()
         })
 
         it('should create the output path to .json file', () => {
-            const idata = new InputData('', output_cmd)
-            idata.createOutputFile()
             expect(fs.existsSync(output_cmd)).toBeTruthy()
         })
     })
